0728a03e33b9b68c1d5d98446afbe7a70d645316
servilla <mark.servilla@gmail.com>
2018-10-19 17:00:36 -0600

    Initial commit of agent_ping

---
+++ agent_ping.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: agent_ping
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    10/19/18
+"""
+import daiquiri
+
+logger = daiquiri.getLogger('agent_ping: ' + __name__)
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
