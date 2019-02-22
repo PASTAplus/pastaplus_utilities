a10a82b6ef5a44af558addb423c61fd4225f87ab
servilla <mark.servilla@gmail.com>
2019-02-21 22:49:24 -0700

    Make eml_getter asyncio

--- eml_gettr.py
+++ eml_gettr.py
@@ -5,17 +5,23 @@
 
 :Synopsis:
     Download EML from packages specified by Solr query to local directory
-    usage: eml_gettr.py --verbose --count=1 -env=production /tmp/eml
+    usage: eml_gettr.py --verbose --count=1 --block_size=10 -env=production /tmp/eml
 
 :Author:
     servilla
 
 :Created:
     12/5/18
+
+:Modified:
+    2/21/19
 """
+import asyncio
 import logging
 import os
 
+import aiohttp
+from aiohttp.client_exceptions import ClientError
 import click
 import daiquiri
 from lxml import etree
@@ -48,14 +54,32 @@ def get_pids(env: str, count: int, fq: str) -> list:
     return pids
 
 
-def get_eml(pid: str, env: str) -> str:
-    eml = None
+async def get_eml(pid: str, pasta: str) -> str:
     package_path = pid.replace('.', '/')
-    eml_url = f'{env}/metadata/eml/{package_path}'
-    r = requests.get(eml_url)
-    if r.status_code == requests.codes.ok:
-        eml = r.text
-    return eml
+    eml_url = f'{pasta}/metadata/eml/{package_path}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(eml_url) as resp:
+            resp.raise_for_status()
+            return await resp.text()
+
+
+async def get_block(pids: list, pasta: str, e_dir: str, verbose: bool):
+    tasks = []
+
+    for pid in pids:
+        tasks.append((pid, loop.create_task(get_eml(pid, pasta))))
+
+    for pid, t in tasks:
+        try:
+            eml = await t
+            file_path = f'{e_dir}/{pid}.xml'
+            with open(file_path, 'w') as f:
+                f.write(eml)
+                msg = f'Writing: {file_path}.xml'
+                if verbose:
+                    click.echo(msg)
+        except (ClientError, IOError):
+            logger.error(f'Failed to access or write: {pid}')
 
 
 env_help = 'PASTA+ environment to query: production, staging, development'
@@ -63,19 +87,21 @@ count_help = 'Number of EML documents to return'
 include_help = 'Include scope'
 exclude_help = 'Exclude scope(s)'
 verbose_help = 'Display event information'
+block_size_help = 'Number of concurrent requests to PASTA'
 
 
 @click.command()
-@click.argument('eml_dir')
+@click.argument('e_dir')
 @click.option('-e', '--env', default='production', help=env_help)
 @click.option('-c', '--count', default=10000, help=count_help)
 @click.option('-i', '--include', default='', help=include_help)
 @click.option('-e', '--exclude', multiple=True, help=exclude_help)
 @click.option('-v', '--verbose', is_flag=True, help=verbose_help)
-def main(eml_dir: str, env: str, count: int, include: tuple, exclude: tuple,
-         verbose: bool):
-    if not os.path.isdir(eml_dir):
-        logger.error(f'EML directory "{eml_dir}" does not exist')
+@click.option('-b', '--block_size', default=5, help=block_size_help)
+def main(e_dir: str, env: str, count: int, include: tuple, exclude: tuple,
+         verbose: bool, block_size: int):
+    if not os.path.isdir(e_dir):
+        logger.error(f'Directory "{e_dir}" does not exist')
         exit(1)
 
     pasta = None
@@ -95,21 +121,15 @@ def main(eml_dir: str, env: str, count: int, include: tuple, exclude: tuple,
     fq += ''.join(['fq=-scope:' + _.strip() + '&' for _ in exclude])
 
     pids = get_pids(pasta, count, fq)
-    npids = len(pids)
-    n = 1
-    for pid in pids:
-        percent = int(n / npids * 100)
-        msg = f'Downloading {pid}: {n}/{npids - n} ({percent}%) - '
-        eml = get_eml(pid, pasta)
-        if eml is not None:
-            with open(f'{eml_dir}/{pid}.xml', 'w') as f:
-                msg += f'writing'
-                f.write(eml)
-        else:
-            msg += f'failed to access'
-        if verbose:
-            click.echo(msg)
-        n += 1
+
+    global loop
+    loop = asyncio.get_event_loop()
+
+    # Execute N blocks of pids concurrently
+    i, j = 0, block_size
+    while i < len(pids):
+        loop.run_until_complete(get_block(pids[i:j], pasta, e_dir, verbose))
+        i, j = j, j + block_size
 
     return 0
 
