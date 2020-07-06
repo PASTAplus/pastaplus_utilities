6fe49acb18fb0f2a925f4a734c688c206ffdc2ae
Mark Servilla <mark.servilla@gmail.com>
2020-07-06 08:07:55 -0600

    Force writing UTF-8; revise docstring count from 10 to 5

--- eml_gettr.py
+++ eml_gettr.py
@@ -5,7 +5,7 @@
 
 :Synopsis:
     Download EML from packages specified by Solr query to local directory
-    usage: eml_gettr.py --verbose --count=1 --block_size=10 --env=production /tmp/eml
+    usage: eml_gettr.py --verbose --count=1 --block_size=5 --env=production /tmp/eml
 
 :Author:
     servilla
@@ -73,7 +73,7 @@ async def get_block(pids: list, pasta: str, e_dir: str, verbose: bool):
         try:
             eml = await t
             file_path = f'{e_dir}/{pid}.xml'
-            with open(file_path, 'w') as f:
+            with open(file_path, 'w', encoding='utf-8') as f:
                 f.write(eml)
                 msg = f'Writing: {file_path}'
                 if verbose:
---
+++ offline_data_entities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: offline_data_entities
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    5/28/20
+"""
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
