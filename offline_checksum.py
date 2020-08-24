f4179b370fa75e915d794a44c39fb12875705620
Mark Servilla <mark.servilla@gmail.com>
2020-08-24 12:04:42 -0600

    Change postgres driver name from postgres to postgresql

---
+++ offline_checksum.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: offline_checksum
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    8/23/20
+"""
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
